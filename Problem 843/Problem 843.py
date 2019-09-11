879ab7612b49280f8e17ab02bc310823579682d3
JalenCato <jalencato23pistons@gmail.com>
2019-09-11 10:51:22 +0800

    Problem 843 update

--- Problem 843/Problem 843.py
+++ Problem 843/Problem 843.py
@@ -1,6 +1,17 @@
-class Solution:
+# each word is 6 letters long
+# one word as secret
+# expectation and filter thoughts are essential
+class Solution(object):
     def findSecretWord(self, wordlist, master):
-        return -1
+        wordSet = set(wordlist)
+        for _ in range(10):
+            if not wordSet:
+                return
+            word = wordSet.pop()
+            cnt = master.guess(word)
+            def check(other):
+                return sum(1 if other[i] == word[i] else 0 for i in range(6)) == cnt
+            wordSet = set(filter(check, wordSet))
 
 
 if __name__ == '__main__':
